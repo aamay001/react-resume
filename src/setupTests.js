5582026e8df000b9a9f7bd629e9609e91cf303b4
Andy Amaya <aamay001@gmail.com>
2017-10-10 20:00:10 -0700

    local storage polyfill for tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,8 @@
 global.requestAnimationFrame = function(callback) {
   setTimeout(callback, 0);
 };
+
+global.localStorage = {
+  setItem: function(name, value){},
+  getItem: function(name){return null}
+}
