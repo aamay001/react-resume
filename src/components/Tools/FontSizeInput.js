750974104b63eecff137912db9b0c6fcb3c1ea6d
= <9aviator@gmail.com>
2019-07-13 20:42:33 +0530

    Added Input For entering font size

--- src/components/Tools/FontSizeInput.js
+++ src/components/Tools/FontSizeInput.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Label, Icon, Button } from 'semantic-ui-react';
+import { Label, Icon, Input } from 'semantic-ui-react';
 import { changeFontSize } from '../../actions/app.actions';
 
 class FontSizeInput extends Component {
@@ -25,34 +25,11 @@ class FontSizeInput extends Component {
           FontSize
         </Label>
 
-        <Button
-          size="small"
-          fluid
-          style={{
-            display: 'block',
-            backgroundColor: 'white',
-            color: 'black',
-          }}
-        >
-          +
-        </Button>
-
-        <input
+        <Input
+          type="number"
           onChange={this.handleFontSizeChange}
           value={fontSize}
         />
-        <Button
-          size="small"
-          fluid
-          style={{
-            display: 'block',
-            backgroundColor: 'white',
-            color: 'black',
-          }}
-        >
-        -
-        </Button>
-
       </div>
     );
   }
