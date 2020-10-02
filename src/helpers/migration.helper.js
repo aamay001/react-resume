37805bfeb1ed0601e39d1a02e7924af118989785
Andy Amaya <aamay001@gmail.com>
2020-10-02 08:25:23 -0700

    Adding migration helper to move old schema to JSON resume schema.

---
+++ src/helpers/migration.helper.js
@@ -0,0 +1,47 @@
+import jsonResume from '../jsonresume-data';
+
+export const migrateSchema = (oldSchema) => {
+  const newSchema = {};
+
+  newSchema.basics = migrateHeader(oldSchema.header);
+  newSchema.work = migrateExperience(oldSchema.experience);
+  newSchema.education = migrateEducation(oldSchema.education);
+  newSchema.skills = mirgrateTechnicalSkills(oldSchema.technicalSkills);
+  newSchema.awards  = migrateCertifications(oldSchema.certification);
+  newSchema.publications = migrateProjects(oldSchema.projects);
+
+  newSchema = addMissingSection(newSchema);
+
+  return newSchema;
+};
+
+const migrateHeader = oldHeader => {
+
+};
+
+const migrateExperience = oldExperience => {
+
+};
+
+const migrateEducation = oldEducation => {
+
+};
+
+const migrateCertifications = oldCertifications => {
+
+};
+
+const mirgrateTechnicalSkills = oldTechSkills => {
+
+};
+
+const migrateProjects = oldProjects => {
+
+};
+
+const addMissingSection = newSchema => ({
+  ...jsonResume,
+  ...newSchema,
+});
+
+export default migrateSchema;
