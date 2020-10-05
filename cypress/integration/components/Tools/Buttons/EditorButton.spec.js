0a5de1ab7c6bf6df7697f4923ccac7083503c175
Jordi Espinosa <jordi.espinosa@sundiogroup.com>
2020-10-05 18:15:28 +0200

    Fix

--- cypress/integration/components/Tools/Buttons/EditorButton.spec.js
+++ cypress/integration/components/Tools/Buttons/EditorButton.spec.js
@@ -12,7 +12,8 @@ context('Click Editor Button Go to Editor page and return to home page', () => {
     cy.get('body > div:nth-child(6) > div > div > div > div').
     contains('⚠️ Auto save to local storage is turned off!').click();
     cy.get('#root > div > div.ui.scale.down.right.very.wide.visible.sidebar > div.ui.massive.borderless.top.attached.menu > a').click();
-    cy.get('#root > div > aside > div > div:nth-child(2) > h1 > span')
-    .contains('JSON Resume');    
+    cy.get('#root > div > aside > div > div:nth-child(2) > h1').contains('JSON Resume'); 
+	cy.get('#root > div > div.ui.scale.down.right.very.wide.visible.sidebar > div.ui.massive.borderless.top.attached.menu > div:nth-child(1)').should('not.exist');
+	
   });
 });
