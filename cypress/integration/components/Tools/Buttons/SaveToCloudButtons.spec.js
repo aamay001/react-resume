2b65b7234c532376bf2f99485d46801ccd28a1d1
Jespicas <espi@espipinxo.com>
2020-10-10 10:04:30 +0200

    Fix according PR Feedback

--- cypress/integration/components/Tools/Buttons/SaveToCloudButtons.spec.js
+++ cypress/integration/components/Tools/Buttons/SaveToCloudButtons.spec.js
@@ -8,20 +8,18 @@ context('Click SaveToCloud buttons', () => {
 
   it('should toggle GoogleDrive SaveToCloudButton', () => {
     cy.get('#root > div > aside > div > div:nth-child(3) > div:nth-child(2) > button.ui.yellow.big.icon.button').click();
-    cy.get('body > div.ui.top.aligned.page.modals.dimmer.transition.visible.active > div > div.content > div > div > p').contains('Google Drive')
-    cy.get('body > div.ui.top.aligned.page.modals.dimmer.transition.visible.active > div > div.actions > button').click();
+    cy.get('body > div.ui.top.aligned.page.modals.dimmer.transition.visible.active > div > div.content > div > div > p').contains('Google Drive');
   });
 
   it('should toggle DropBox SaveToCloudButton', () => {
     cy.get('#root > div > aside > div > div:nth-child(3) > div:nth-child(2) > button.ui.twitter.big.icon.button').click();
-    cy.get('body > div.ui.top.aligned.page.modals.dimmer.transition.visible.active > div > div.content > div > div > p').contains('Dropbox')
-    cy.get('body > div.ui.top.aligned.page.modals.dimmer.transition.visible.active > div > div.actions > button').click();
+    cy.get('body > div.ui.top.aligned.page.modals.dimmer.transition.visible.active > div > div.content > div > div > p').contains('Dropbox');
   });
 
   it('should toggle OneDrive SaveToCloudButton', () => {
     cy.get('#root > div > aside > div > div:nth-child(3) > div:nth-child(2) > button.ui.facebook.big.icon.button').click();
-    cy.get('body > div.ui.top.aligned.page.modals.dimmer.transition.visible.active > div > div.content > div > div > p').contains('OneDrive')
-    cy.get('body > div.ui.top.aligned.page.modals.dimmer.transition.visible.active > div > div.actions > button').click();
+    cy.get('body > div.ui.top.aligned.page.modals.dimmer.transition.visible.active > div > div.content > div > div > p').contains('OneDrive');
+
   });
 
 });
