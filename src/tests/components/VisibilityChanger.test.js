cb9eeded2bc3b71637373e7bc8737ed995f97f51
Andy Amaya <amayaa@MacBook-Pro.local>
2020-10-10 07:08:48 -0700

    Including react-scripts test in pipeline.

--- src/tests/components/VisibilityChanger.test.js
+++ src/tests/components/VisibilityChanger.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '../store';
-import VisibilityChanger from '../components/Tools/VisibilityChanger';
-import Resume from '../components/Resume';
+import store from '../../store';
+import VisibilityChanger from '../../components/Tools/VisibilityChanger';
+import Resume from '../../components/Resume';
 
 // TODO: add tests for checking ItemToggleButton[data-testid='showSkillLevel'] button functioning
 // TODO: include MoreVisibilityButton within tests
