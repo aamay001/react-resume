342b0d91760d98060028a1ea2458547273b80ffc
Andy Amaya <aamay001@gmail.com>
2020-10-15 09:52:13 -0700

    Updates `DownloadButton` test.

--- cypress/integration/components/Tools/Buttons/DownloadButton.spec.js
+++ cypress/integration/components/Tools/Buttons/DownloadButton.spec.js
@@ -1,22 +1,18 @@
-/// <reference types="cypress" />
+// <reference types="cypress" />
+import resumeData from '../../../../../src/migratedresume-data';
 
 context('Click Download Button', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-  
-    it('should click Download Button', () => {
-      cy.get('#root > div > aside > div > div:nth-child(9) > a[download="resume.json"]');
-      cy.fixture('downloadButtonfile').as('expectedValueinAttribute').then(($expectedValueinAttribute) =>
-      {          
-      cy.get('#root > div > aside > div > div:nth-child(9) > a')      
-      .should('have.attr', 'href').and('include', $expectedValueinAttribute)
-      .then((href) => {
-        cy.visit(href)
-      })
-    })
-    });
+  beforeEach(() => {
+    cy.visit('/');
   });
-  
-  
-  
+
+  it('should click Download Button', () => {
+    const resumePayload = `data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(resumeData, null, '\t'))}`;
+    cy.get('#root > div > aside > div > div:nth-child(9) > a[download="resume.json"]');
+    cy.get('#root > div > aside > div > div:nth-child(9) > a')
+      .should('have.attr', 'href').and('include', resumePayload)
+      .then((href) => {
+        cy.visit(href);
+      });
+  });
+});
