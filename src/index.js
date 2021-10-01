e47ec35494d41b2b8db7a5f3538e804d3b905584
shakyaprasen <kajiprasen@gmail.com>
2021-10-01 12:22:35 +0545

    DarkModeForToast: Add darkmode for toast and update react-toastify

--- src/index.js
+++ src/index.js
@@ -2,29 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { toast } from 'react-toastify';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
 
-toast.configure({
-  position: 'top-center',
-  autoClose: 3000,
-  hideProgressBar: false,
-  newestOnTop: false,
-  closeOnClick: true,
-  closeButton: false,
-  rtl: false,
-  pauseOnVisibilityChange: true,
-  draggable: true,
-  pauseOnHover: true,
-  progressStyle: {
-    background: 'lightgray',
-  },
-  bodyClassName: 'resume-toast-body',
-});
 
 ReactDOM.render(
   <Provider store={store}>
