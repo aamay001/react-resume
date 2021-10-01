64da2fc3c8032d94d3ecba40b7abc9edc9aecc36
shakyaprasen <44523852+shakyaprasen@users.noreply.github.com>
2021-10-01 21:09:50 +0545

    DarkModeForToast: Add darkmode for toast and update react-toastify (#109)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch, Router } from 'react-router';
 import { connect } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import { createBrowserHistory } from 'history';
 import classNames from 'classnames';
 import { constants } from './config';
@@ -24,6 +25,23 @@ const App = ({
   }
   return (
     <div className={classNames('App', { darkMode })}>
+    <ToastContainer
+      position='top-center'
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={true}
+      closeButton={false}
+      rtl={false}
+      pauseOnVisibilityChange
+      draggable
+      pauseOnHover
+      progressStyle={{
+        background: 'lightgray',
+      }}
+      bodyClassName='resume-toast-body'
+      theme={darkMode ? 'dark' : 'light'}
+    />
       <Router history={history}>
         <Switch>
           <Route exact path={ROUTES.HOME.PATH} component={Home} />
