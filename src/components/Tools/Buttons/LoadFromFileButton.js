786db097658ab6150e971cc2c3d15baa017dfb29
Andy Amaya <contact@andyamaya.com>
2024-04-18 12:33:33 -0700

    Update LoadFromFile from class component to functional.

--- src/components/Tools/Buttons/LoadFromFileButton.js
+++ src/components/Tools/Buttons/LoadFromFileButton.js
@@ -1,79 +1,82 @@
-import React, { Component, createRef } from 'react';
+import React, { createRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Label, Icon, Button } from 'semantic-ui-react';
 import { toast } from 'react-toastify';
 import { updateResume } from '../../../actions/app.actions';
 
-class LoadFromFileButton extends Component {
-  fileRef = createRef();
+function LoadFromFileButton({ dispatch, autoSave }) {
+  // eslint-disable-next-line prefer-const
+  let fileRef = createRef();
 
-  onFileChange = ({ target }) => {
+  const onFileChange = useCallback(({ target }) => {
     const file = target.files && target.files.length && target.files[0];
 
+    const onFileError = () => {
+      toast('😟 Something went wrong while loading from file!', { autoClose: false });
+    };
+
+    const onFileRead = ({ target: t1 }) => {
+      const { result } = t1;
+
+      if (!result) {
+        toast('😟 No data loaded from file!', { autoClose: false });
+        return;
+      }
+
+      try {
+        const resume = JSON.parse(result);
+
+        dispatch(updateResume(resume, autoSave));
+
+        toast('🙌 Resume loaded from file!');
+      } catch (e) {
+        toast('😟 Could not parse data file!', { autoClose: false });
+      }
+    };
+
+    const resetFileRef = () => {
+      fileRef = null;
+      fileRef = createRef();
+    };
+
+    const readFile = (f1) => {
+      const fileReader = new FileReader();
+      fileReader.onload = onFileRead;
+      fileReader.onerror = onFileError;
+      fileReader.onloadend = resetFileRef;
+      fileReader.readAsText(f1);
+    };
+
     if (file) {
-      this.readFile(file);
+      readFile(file);
     }
-  }
+  }, [fileRef]);
 
-  onFileError = () => {
-    toast('😟 Something went wrong while loading from file!', { autoClose: false });
-  }
-
-  onFileRead = ({ target }) => {
-    const { dispatch, autoSave } = this.props;
-    const { result } = target;
-
-    if (!result) {
-      toast('😟 No data loaded from file!', { autoClose: false });
-      return;
-    }
-
-    try {
-      const resume = JSON.parse(result);
-
-      dispatch(updateResume(resume, autoSave));
-
-      toast('🙌 Resume loaded from file!');
-    } catch (e) {
-      toast('😟 Could not parse data file!', { autoClose: false });
-    }
-  }
-
-  readFile(file) {
-    const fileReader = new FileReader();
-
-    fileReader.onload = this.onFileRead;
-    fileReader.onerror = this.onFileError;
-    fileReader.readAsText(file);
-  }
-
-  render() {
-    return (
-      <div className="json-resume-tool">
-        <Label size="big" basic>
-          <Icon name="upload" />
-          Load From File
-        </Label>
-        <Button
-          content="Select File"
-          onClick={() => this.fileRef.current.click()}
-          size="large"
-          fluid
-          style={{
-            display: 'block',
-            backgroundColor: 'white',
-            color: 'black',
-          }}
-        />
-        <input
-          ref={this.fileRef}
-          type="file"
-          hidden
-          onChange={this.onFileChange}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="json-resume-tool">
+      <Label size="big" basic>
+        <Icon name="upload" />
+        Load From File
+      </Label>
+      <Button
+        content="Select File"
+        onClick={() => fileRef.current.click()}
+        size="large"
+        fluid
+        style={{
+          display: 'block',
+          backgroundColor: 'white',
+          color: 'black',
+        }}
+      />
+      <input
+        ref={fileRef}
+        type="file"
+        hidden
+        onChange={onFileChange}
+      />
+    </div>
+  );
 }
 
 LoadFromFileButton.defaultProps = {
